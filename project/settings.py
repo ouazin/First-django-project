a2657006b08418b6e0e9545e1c1fd8a80c94743b
OUAZIN Nabil <nabilouazin91@gmail.com>
2022-09-03 21:07:06 +0100

    loading static files

--- project/settings.py
+++ project/settings.py
@@ -125,6 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Added
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/',
+]
+
+# MEDIA
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
